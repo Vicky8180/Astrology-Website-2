733084dd839151c7800a77b626fb9f3256703e7f
Anoop Yadav <76256436+Vicky8180@users.noreply.github.com>
2024-03-20 01:21:17 +0530

    Update cors4 server.js

--- backend/server.js
+++ backend/server.js
@@ -121,7 +121,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../my-app/build', 'index.html'));
 });
 
-const server=app.listen(process.env.PORT || 5000,()=>{
+const server=app.listen(5000,()=>{
     console.log("its running ")
 })
 
