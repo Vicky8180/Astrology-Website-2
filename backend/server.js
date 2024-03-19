47ae689f103ed90e5ff6be507c0b0fe4d33d7306
Anoop Yadav <76256436+Vicky8180@users.noreply.github.com>
2024-03-20 00:37:16 +0530

    Update cors 2 server.js

--- backend/server.js
+++ backend/server.js
@@ -52,7 +52,12 @@ app.use(cors({
     credentials: true,
     
 }));
-
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://astrologywithanoop.netlify.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+})
 
 ////////////////////////////////
 app.use(express.urlencoded({ extended: true }));
